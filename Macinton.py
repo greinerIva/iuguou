f8dd74b14486344562406c37856d82c12ecd747e
greinerIva <122392512+greinerIva@users.noreply.github.com>
2023-03-31 21:16:43 +0300

    Update Macinton.py

--- Macinton.py
+++ Macinton.py
@@ -1,19 +1,38 @@
 from tkinter import *
 from tkinter.filedialog import asksaveasfile
+from keyboard import add_hotkey
 
 
 def save_file():
-   f = asksaveasfile(initialfile = 'Untiled.macinton0',
+    f = asksaveasfile(initialfile = 'Untiled.macinton0',
 defaultextension=".macinton0",filetypes=[("All Files","*.*"),("Macinton 0","*.macinton0")])
 
+
+comms = []
+strscode = 1
+
+def newtake(event):
+    global strscode, stringsare, windo
+    if strscode == 1:
+        comms.append(code.get())
+    newenter = Entry(windo, width=128)
+    newenter.grid(column=1, row=stringsare+1)
+    code.grid(column=1, row=stringsare)
+    stringsare += 1
+    strscode += 1
+    newenter.bind('<Return>', newtake)
+    return stringsare, strscode
+    
+
+stringsare = 1
 windo = Tk()
 windo.title('Macinton 0')
 windo.geometry('2048x1024')
-btn= Button(windo, text= "Save", command= lambda:save_file())
-btn.grid(column=1, row=1)
 
-isok = True
-stringsare = 3
-code = Entry(windo, width=12)
+add_hotkey('ctrl + s', save_file, args =())
+
+
+code = Entry(windo, width=128)
 code.grid(column=1, row=stringsare)
+code.bind("<Return>", newtake)
 windo.mainloop()
